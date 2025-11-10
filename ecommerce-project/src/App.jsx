1e2e49e0eee9b621d69431a3ba5dc61476f6dcaa
Flinn Alexnader Hancock <89704618+FHZergursh@users.noreply.github.com>
2025-11-10 23:03:33 +0000

    implement react router
    title

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import { HomePage } from './pages/HomePage'
+import {Route, Routes} from 'react-router';
 
 function App() {
 
   return (
     <>
-    <HomePage />
+    <Routes>
+      <Route path='/' element={<HomePage />}></Route>
+      <Route path='/checkout' element={<div>test</div>}></Route>
+    </Routes>
     </>
 
   )
