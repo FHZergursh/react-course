70d496c2b9a6f1fc287e3538ec21016268bac8ed
Zergursh <flinnh11@gmail.com>
2025-11-17 00:22:31 +0000

    fetch data from backend using axios
    title

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
 import "./HomePage.css";
 import { products } from "../../starting-code/data/products.js";
 import Header from "../components/Header";
+import axios from 'axios'
 
 export function HomePage() {
+
+  axios.get('http://localhost:3000/api/products')
+    .then((response) => {
+      console.log(response.data)
+    })
+
+
   return (
     <>
       <title>Home Page</title>
